bc58e80009477484d91d8d0082b521ab384259c4
Hanh Nguyen Tai <nthanh@mytrax.co.jp>
2016-01-15 17:45:55 +0700

    co ban listview

--- 01. Basic/02. Layout/ListView/app/src/main/java/com/example/nthanh/listview/MainActivity.java
+++ 01. Basic/02. Layout/ListView/app/src/main/java/com/example/nthanh/listview/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.nthanh.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Array of strings...
+    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X"};
+    TextView txtSelectItem ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtSelectItem = (TextView) findViewById(R.id.lbSelectedItem);
+        //setup adapter array
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
+        //event click
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                txtSelectItem.setText("Vi tri: " + position + " | value: " + mobileArray[position]);
+            }
+        });
+
+        //event long clic
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), " " + position, Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
     }
 }
