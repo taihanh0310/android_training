1c4eb39313d6a924ae7be0961ef64becea37a7ef
Hanh Nguyen Tai <taihanh0310@gmail.com>
2016-03-16 22:08:50 +0700

    flashlight

---
+++ 02.Demo/Flashlight/app/src/main/java/com/example/nthanh/flashlight/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.nthanh.flashlight;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.security.Policy;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnSwitch;
+    private Camera camera;
+    private boolean isFlashOn, hasFlash;
+    Parameters params;
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //flash switch button
+        btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
+
+        //first check if device is support flashlight or not
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash) {
+            //device doesn't support flash
+            //show alert message and close this application
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        //get Camera
+        getCamera();
+
+        //displaying button image
+        toggleButtonImage();
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlashOn) {
+                    turnOffFlash();
+                } else {
+                    turnOnFlash();
+                }
+            }
+        });
+    }
+
+    //override default method state
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //on resume turn on the flash
+        if (hasFlash) {
+            turnOnFlash();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //on starting the app get the camera params
+        getCamera();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // on stop release the camera
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void getCamera() {
+        if (camera == null) {
+            try {
+                //open camera
+                camera = android.hardware.Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Camera error.Error: ", e.getMessage());
+            }
+        }
+    }
+
+
+    public void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+
+            // changing button/switch image
+            toggleButtonImage();
+        }
+    }
+
+    // Turning Off flash
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+            isFlashOn = false;
+
+            // changing button/switch image
+            toggleButtonImage();
+        }
+    }
+
+    private void playSound() {
+        if (isFlashOn) {
+            mp = MediaPlayer.create(MainActivity.this, R.raw.light_switch_off);
+        } else {
+            mp = MediaPlayer.create(MainActivity.this, R.raw.light_switch_on);
+        }
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // TODO Auto-generated method stub
+                mp.release();
+            }
+        });
+        mp.start();
+    }
+
+    private void toggleButtonImage() {
+        if (isFlashOn) {
+            btnSwitch.setImageResource(R.drawable.btn_switch_on);
+        } else {
+            btnSwitch.setImageResource(R.drawable.btn_switch_off);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,3 +9,7 @@ http://www.androidhive.info/2012/03/android-building-audio-player-tutorial/
 # http://zetcode.com/
 # http://www.tutorialsbuzz.com/2014/08/android-listview-collection.html
 # http://www.bogotobogo.com/Android/android7dvancedListView.php
+
+### Tài liệu những bài android hay nhất
+> http://www.androidhive.info/2013/04/android-developing-flashlight-application/
+> http://android.okhelp.cz/turn-screen-on-off-android-sample-code/
